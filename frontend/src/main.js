4900407d36852a122b572044d653114d5eaf97e4
willelol04 <147752701+willelol04@users.noreply.github.com>
2026-01-14 21:18:53 +0100

    adjust CORS

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,3 +4,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+console.log("yoo")
+
+const promise = await fetch("http://localhost:8000/")
+const returnedText = await promise.json();
+console.log(returnedText.message);
+
+const fastapiParagraph = document.querySelector("#fastapi-test");
+fastapiParagraph.innerText = returnedText.message;
